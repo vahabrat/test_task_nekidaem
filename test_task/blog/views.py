5e97946449131f15e27cc5164dfb02ddd6ad0d00
vahabrat <62399153+vahabrat@users.noreply.github.com>
2020-11-23 00:02:15 +0300

    creating form for Posts

--- test_task/blog/views.py
+++ test_task/blog/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
 
+    if request.method == 'POST':
+        Post.objects.create(title=request.POST['title'],text=request.POST['post_text'],user=request.user)
+    else:
+        pass
     posts = Post.objects.filter(user = request.user)
-
 
     return render(request, 'blog/post_list.html',{'posts': posts})
 
@@ -19,11 +23,13 @@ def users_list(request):
             request.user.subscribed_on.add(int(request.POST['subscribe_to']))
         elif action == 'unsubscribe':
             request.user.subscribed_on.remove(int(request.POST['unsubscribe_from']))
+            removal_post_read = Post.objects.filter(user_id=int(request.POST['unsubscribe_from']))
+            request.user.is_read_list.remove(*removal_post_read)
 
     print(list(request.user.subscribed_on.all()))
 
-    users = get_user_model().objects.all()
-
+    #users = get_user_model().objects.all()
+    users = get_user_model().objects.exclude(id=request.user.id)
     return render(request, 'blog/users_list.html',{'users': users})
 
 
@@ -50,17 +56,22 @@ def news(request):
 
     some_function()
 
-    #posts = Post.objects.filter(user__in=request.user)
+    if request.method == 'POST':
+        action = request.POST['action']
+        if action == 'as_un_read':
+
+            request.user.is_read_list.remove(int(request.POST['as_un_read']))
+        if action == 'as_read':
+
+            request.user.is_read_list.add(int(request.POST['as_read']))
+
+
     users = request.user.subscribed_on.all()
     posts = Post.objects.filter(user__in=users).order_by('-date_creation')
 
-
-
-
-
-
-
-
-
     return render(request, 'blog/news.html',{'posts': posts})
+    """return JsonResponse({'posts': [{
+        "id": post.id,
+        "title": post.title,
+    } for post in posts]})"""
 # Create your views here.
